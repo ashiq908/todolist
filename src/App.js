30d215fd3f71e44a50b8884127eeba314b24cf7f
ashiq908 <ashikashok37@gmail.com>
2024-09-09 18:49:45 +0530

    loaded

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import './App.css';
+
+const App=()=>{
+    const [input,setInput]=useState('')
+    const [todos,setTodos]=useState([])
+    const addTodo=()=>{
+        if(input!=='')
+        {setTodos([...todos,input]);
+        setInput('');}
+    }
+    const editTodo=(upTodo,id)=>{
+        const updTodo=todos.map((todo,index)=>(
+            index===id ? upTodo:todo
+        ));
+        setTodos(updTodo);
+    }
+    const deleTodo=(id)=>{
+        setTodos(todos.filter((todo,index)=>index!==id));
+    }
+    return (
+        <div className='App'>
+            <h1>To-do App</h1>
+            <input
+            value={input}
+            onChange={(e)=>setInput(e.target.value)}
+            placeholder='To-do'
+            maxLength={10}
+            />
+            <button onClick={addTodo}>Add</button>
+            <ul>
+                {todos.map((todo,id)=>(
+                    <li key={id}>
+                         <input
+                         value={todo}
+                         onChange={(e)=>editTodo(id,e.target.value)}
+                         />
+                         <button onClick={()=>editTodo(id)}>Edit</button>
+                        <button onClick={()=>deleTodo(id)}>Delete</button>
+                        </li>
+                )
+                )}
+            </ul>
+            </div>
+    );
+}
+
+export default App;
