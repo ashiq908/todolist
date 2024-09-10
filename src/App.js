5f5814aef3cc9851a4b362958025789b1f07d07b
ashiq908 <ashikashok37@gmail.com>
2024-09-10 12:32:53 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,48 +1,74 @@
 import { useState } from 'react';
 import './App.css';
 
-const App=()=>{
-    const [input,setInput]=useState('')
-    const [todos,setTodos]=useState([])
-    const addTodo=()=>{
-        if(input!=='')
-        {setTodos([...todos,input]);
-        setInput('');}
+const App = () => {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+  const [editInput, setEditInput] = useState('');
+
+  const addTodo = () => {
+    if (input !== '') {
+      setTodos([...todos, input]);
+      setInput('');
     }
-    const editTodo=(upTodo,id)=>{
-        const updTodo=todos.map((todo,index)=>(
-            index===id ? upTodo:todo
-        ));
-        setTodos(updTodo);
-    }
-    const deleTodo=(id)=>{
-        setTodos(todos.filter((todo,index)=>index!==id));
-    }
-    return (
-        <div className='App'>
-            <h1>To-do App</h1>
-            <input
-            value={input}
-            onChange={(e)=>setInput(e.target.value)}
-            placeholder='To-do'
-            maxLength={10}
-            />
-            <button onClick={addTodo}>Add</button>
-            <ul>
-                {todos.map((todo,id)=>(
-                    <li key={id}>
-                         <input
-                         value={todo}
-                         onChange={(e)=>editTodo(id,e.target.value)}
-                         />
-                         <button onClick={()=>editTodo(id)}>Edit</button>
-                        <button onClick={()=>deleTodo(id)}>Delete</button>
-                        </li>
-                )
-                )}
-            </ul>
-            </div>
+  };
+
+  const startEditing = (id, currentTodo) => {
+    setEditIndex(id);
+    setEditInput(currentTodo);
+  };
+
+  const saveEdit = (id) => {
+    editTodo(editInput, id);
+    setEditIndex(null); // Exit edit mode
+  };
+
+  const editTodo = (upTodo, id) => {
+    const updatedTodos = todos.map((todo, index) =>
+      index === id ? upTodo : todo
     );
-}
+    setTodos(updatedTodos);
+  };
+
+  const deleTodo = (id) => {
+    setTodos(todos.filter((todo, index) => index !== id));
+  };
+
+  return (
+    <div className='App'>
+      <h1>To-do App</h1>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder='To-do'
+        maxLength={10}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, id) => (
+          <li key={id}>
+            {editIndex === id ? (
+              <input
+                value={editInput}
+                onChange={(e) => setEditInput(e.target.value)}
+              />
+            ) : (
+              <span>{todo}</span>
+            )}
+
+            {editIndex === id ? (
+              <button onClick={() => saveEdit(id)}>Save</button>
+            ) : (
+              <button onClick={() => startEditing(id, todo)}>Edit</button>
+            )}
+
+            <button onClick={() => deleTodo(id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
